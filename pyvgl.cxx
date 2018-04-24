b711dbac725702a89897bb289fe7c2fb430894f1
Robert Wagner <robert.wagner@visionsystemsinc.com>
2018-04-24 13:34:28 -0400

    added homg_point_3d

--- pyvgl.cxx
+++ pyvgl.cxx
@@ -5,6 +5,7 @@
 #include <vgl/vgl_point_3d.h>
 #include <vgl/algo/vgl_rotation_3d.h>
 #include <vgl/vgl_homg_point_2d.h>
+#include <vgl/vgl_homg_point_3d.h>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/operators.h>
@@ -72,6 +73,31 @@ double getitem_homg_2d(T const& a, long i)
     return a.y();
   }
   else if (i==2) {
+    return a.w();
+  }
+  else {
+    throw py::index_error("index out of range");
+  }
+  return 0; // to avoid compiler warning
+}
+
+template<class T>
+double getitem_homg_3d(T const& a, long i)
+{
+  // wrap around
+  if (i < 0) {
+    i += 3;
+  }
+  if (i==0) {
+    return a.x();
+  }
+  else if (i==1) {
+    return a.y();
+  }
+  else if (i==2) {
+    return a.z();
+  }
+  else if (i==3) {
     return a.w();
   }
   else {
@@ -155,6 +181,35 @@ T type_from_buffer_homg_2d(py::array_t<BUFF_T> b)
   return T(x,y,w);
 }
 
+template <class T, class BUFF_T>
+T type_from_buffer_homg_3d(py::array_t<BUFF_T> b)
+{
+  py::buffer_info info = b.request();
+  if (info.format != py::format_descriptor<BUFF_T>::format()) {
+    throw std::runtime_error("Incompatible scalar type");
+  }
+  if (info.ndim != 1) {
+    throw std::runtime_error("Expecting a 1-dimensional vector");
+  }
+  const size_t num_elements = info.shape[0];
+  if (num_elements == 2 || num_elements > 4) {
+    throw std::runtime_error("Expecting 3-d or 4-d input vector");
+  }
+  // in-place constructor
+  const BUFF_T* data_ptr = static_cast<BUFF_T*>(info.ptr);
+  const size_t stride = info.strides[0] / sizeof(BUFF_T);
+  BUFF_T x = *data_ptr;
+  BUFF_T y = *(data_ptr + stride);
+  BUFF_T z = *(data_ptr + 2*stride);
+
+  if (num_elements == 3)
+    return T(x,y,z);
+
+  BUFF_T w = *(data_ptr + 3*stride);
+
+  return T(x,y,z,w);
+}
+
 void wrap_vgl(py::module &m)
 {
   py::class_<vgl_point_2d<double> > (m, "point_2d")
@@ -206,7 +261,6 @@ void wrap_vgl(py::module &m)
     .def("as_quaternion", &vgl_rotation_3d<double>::as_quaternion)
     .def(py::self * py::self);
 
-
   py::class_<vgl_homg_point_2d<double> > (m, "homg_point_2d")
       .def(py::init<double,double>())
       .def(py::init<double,double,double>())
@@ -216,6 +270,18 @@ void wrap_vgl(py::module &m)
       .def_property_readonly("x", (double (vgl_homg_point_2d<double>::*)() const) &vgl_homg_point_2d<double>::x)
       .def_property_readonly("y", (double (vgl_homg_point_2d<double>::*)() const) &vgl_homg_point_2d<double>::y)
       .def_property_readonly("w", (double (vgl_homg_point_2d<double>::*)() const) &vgl_homg_point_2d<double>::w)
+      .def(py::self - py::self);
+
+  py::class_<vgl_homg_point_3d<double> > (m, "homg_point_3d")
+      .def(py::init<double,double,double>())
+      .def(py::init<double,double,double,double>())
+      .def(py::init(&type_from_buffer_homg_3d<vgl_homg_point_3d<double>, double>))
+      .def("__len__", [](vgl_homg_point_3d<double>){return (size_t)4;})
+      .def("__getitem__", getitem_homg_3d<vgl_homg_point_3d<double> >)
+      .def_property_readonly("x", (double (vgl_homg_point_3d<double>::*)() const) &vgl_homg_point_3d<double>::x)
+      .def_property_readonly("y", (double (vgl_homg_point_3d<double>::*)() const) &vgl_homg_point_3d<double>::y)
+      .def_property_readonly("z", (double (vgl_homg_point_3d<double>::*)() const) &vgl_homg_point_3d<double>::z)
+      .def_property_readonly("w", (double (vgl_homg_point_3d<double>::*)() const) &vgl_homg_point_3d<double>::w)
       .def(py::self - py::self);
 }
 }}
