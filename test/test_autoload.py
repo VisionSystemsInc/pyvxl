c17fc008f580919cd62200b9fe3e82db2be01411
Noah Johnson <noah.johnson@visionsystemsinc.com>
2019-05-29 12:46:51 -0400

    Added contrib modules to autoload test

--- test/test_autoload.py
+++ test/test_autoload.py
@@ -1,12 +1,11 @@
 import unittest
 
-try:
-  import numpy as np
-except:
-  np = None
-
 
 class TestAutoLoad(unittest.TestCase):
+  """
+  Test that importing vxl auto-loads all of the
+  modules inside vxl.
+  """
 
   def test_imports(self):
     import vxl
@@ -17,6 +16,14 @@ class TestAutoLoad(unittest.TestCase):
     vxl.vpgl
     vxl.vpgl.algo
     vxl.contrib
+    vxl.contrib.bpgl
+    vxl.contrib.bpgl.algo
+    vxl.contrib.brad
+    vxl.contrib.brip
+    vxl.contrib.bvxm
+    vxl.contrib.bvxm.algo
+    vxl.contrib.sdet
+    vxl.contrib.sdet.algo
 
 
 if __name__ == '__main__':
