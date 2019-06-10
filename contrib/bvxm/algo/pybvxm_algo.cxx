0904e3ae1320ce73746ef1d32bb6299746118740
Noah Johnson <noah.johnson@visionsystemsinc.com>
2019-06-10 13:13:15 -0400

    Updated bvxm.create_scene_large_scale to reflect API change to vgl_box_2d instead of kml string

--- contrib/bvxm/algo/pybvxm_algo.cxx
+++ contrib/bvxm/algo/pybvxm_algo.cxx
@@ -63,7 +63,7 @@ void wrap_bvxm_algo(py::module &m)
 
   /* m.def("create_scene", &bvxm_create_scene); */
   m.def("create_scene_large_scale", &bvxm_create_scene_xml_large_scale,
-        py::arg("roi_kml"), py::arg("scene_root"),
+        py::arg("bbox"), py::arg("scene_root"),
         py::arg("world_dir"), py::arg("dem_folder"),
         py::arg("world_size_in")=500.0, py::arg("voxel_size")=1.0,
         py::arg("height_diff")=120.0, py::arg("height_sub")=25.0,
