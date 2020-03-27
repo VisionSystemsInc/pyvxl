df16c6e8fb3220fd4f8bad63c6072d49499edaa2
Drew Gilliam <drew@visionsystemsinc.com>
2020-03-27 14:25:39 -0400

    update bsgm tests for new struct/dict routines

--- test/test_pybsgm.py
+++ test/test_pybsgm.py
@@ -1,20 +1,40 @@
+import copy
 import unittest
-from utils import VxlBase
+import utils
 
 from vxl import vgl
 from vxl.contrib import bsgm
 
 
-class BsgmBase(VxlBase):
+class BsgmBase(utils.VxlBase):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.default_data = dict()
 
+  def _set_data_full(self):
+    data = copy.deepcopy(self.default_data)
+    data = utils.update_nested_dict(data, getattr(self, 'set_data', {}))
+    return data
+
   def test_create(self):
     instance = self.cls()
     self.assertIsInstance(instance, self.cls)
     self.assertAttributes(instance, self.default_data)
+
+  @utils.skipUnlessAttr('set_data')
+  def test_init(self):
+    init_data = self._set_data_full()
+    instance = self.cls(**init_data)
+    self.assertIsInstance(instance, self.cls)
+    self.assertAttributes(instance, init_data)
+
+  @utils.skipUnlessAttr('set_data')
+  def test_set(self):
+    instance = self.cls()
+    instance.set(**self.set_data)
+    new_data = self._set_data_full()
+    self.assertAttributes(instance, new_data)
 
 
 class bsgm_disparity_estimator_params(BsgmBase, unittest.TestCase):
@@ -41,6 +61,13 @@ class bsgm_disparity_estimator_params(BsgmBase, unittest.TestCase):
         'print_timing': False,
     }
 
+    self.set_data = {
+        'p1_scale': 5.0,
+        'p2_scale': 5.0,
+        'census_tol': 4,
+        'census_rad': 4,
+    }
+
 
 class bsgm_pairwise_params(BsgmBase, unittest.TestCase):
 
@@ -48,9 +75,28 @@ class bsgm_pairwise_params(BsgmBase, unittest.TestCase):
     super().__init__(*args, **kwargs)
     self.cls = bsgm.pairwise_params
 
+    default_shadow_thresh = 20
+    default_quad_interp = False
     self.default_data = {
-        'shadow_thresh': 20,
-        'quad_interp': False,
+        'de_params': {
+            'use_16_directions': False,
+            'p1_scale': 1.0,
+            'p2_scale': 1.0,
+            'use_gradient_weighted_smoothing': True,
+            'max_grad': 32.0,
+            'perform_quadratic_interp': default_quad_interp,
+            'error_check_mode': 1,
+            'shadow_thresh': default_shadow_thresh,
+            'bias_weight': 0.0,
+            'bias_dir': vgl.vector_2d_float(1.0, 0.0),
+            'census_weight': 0.3,
+            'xgrad_weight': 0.7,
+            'census_tol': 2,
+            'census_rad': 2,
+            'print_timing': False,
+        },
+        'shadow_thresh': default_shadow_thresh,
+        'quad_interp': default_quad_interp,
         'multi_scale_mode': 0,
         'active_disparity_factor': 0.5,
         'downscale_exponent': 2,
@@ -63,25 +109,22 @@ class bsgm_pairwise_params(BsgmBase, unittest.TestCase):
         'min_neighbors': 3,
         'max_neighbors': 5,
         'neighbor_dist_factor': 3.0,
-        "coarse_dsm_disparity_estimate": True,
+        'coarse_dsm_disparity_estimate': True,
     }
 
-    self.default_data['de_params'] = {
-        'use_16_directions': False,
-        'p1_scale': 1.0,
-        'p2_scale': 1.0,
-        'use_gradient_weighted_smoothing': True,
-        'max_grad': 32.0,
-        'perform_quadratic_interp': self.default_data['quad_interp'],
-        'error_check_mode': 1,
-        'shadow_thresh': self.default_data['shadow_thresh'],
-        'bias_weight': 0.0,
-        'bias_dir': vgl.vector_2d_float(1.0, 0.0),
-        'census_weight': 0.3,
-        'xgrad_weight': 0.7,
-        'census_tol': 2,
-        'census_rad': 2,
-        'print_timing': False,
+    set_shadow_thresh = 0
+    set_quad_interp = True
+    self.set_data = {
+        'de_params': {
+            'p1_scale': 5.0,
+            'p2_scale': 10.0,
+            'perform_quadratic_interp': set_quad_interp,
+            'shadow_thresh': set_shadow_thresh,
+        },
+        'shadow_thresh': set_shadow_thresh,
+        'quad_interp': set_quad_interp,
+        'multi_scale_mode': 1,
+        'active_disparity_factor': 1.0,
     }
 
 
