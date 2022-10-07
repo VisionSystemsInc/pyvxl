8d15aae721dcc02e7a38c5cf464693343b417ad2
Drew Gilliam <drew@visionsystemsinc.com>
2022-10-07 14:23:25 -0400

    vgl_algo_intersection for multiple rays

--- vgl/algo/pyvgl_algo.cxx
+++ vgl/algo/pyvgl_algo.cxx
@@ -5,8 +5,13 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
+#include <vgl/vgl_point_3d.h>
+#include <vgl/vgl_ray_3d.h>
+#include <vnl/vnl_matrix.h>
+
 #include <vgl/algo/vgl_compute_rigid_3d.h>
 #include <vgl/algo/vgl_compute_similarity_3d.h>
+#include <vgl/algo/vgl_intersection.h>
 #include <vgl/algo/vgl_rotation_3d.h>
 
 #include "../../pyvxl_util.h"
@@ -14,6 +19,27 @@
 namespace py = pybind11;
 
 namespace pyvxl { namespace vgl { namespace algo {
+
+template<typename T>
+void wrap_vgl_algo_intersection(py::module &m)
+{
+  m.def("intersection",
+    [](std::vector<vgl_ray_3d<T> > const& rays,
+       vnl_matrix<T> const& covariance)
+    {
+      vgl_point_3d<T> pt;
+      bool success = (!covariance.empty())
+                   ? vgl_intersection(rays, covariance, pt)
+                   : vgl_intersection(rays, pt);
+      if (!success) {
+        throw std::runtime_error("rays do not intersect at well-conditioned solution");
+      }
+      return pt;
+    },
+    py::arg("rays"), py::arg("covariance")=vnl_matrix<T>())
+
+  ;
+}
 
 void wrap_vgl_algo(py::module &m)
 {
@@ -48,6 +74,8 @@ void wrap_vgl_algo(py::module &m)
     .def(py::self * py::self)
     .def(py::self == py::self)
     ;
+
+  wrap_vgl_algo_intersection<double>(m);
 
 }
 }}}
